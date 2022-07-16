2898eb7f5603736bb5deba685f52e44a1450744f
Agus <agustinamiceli@hotmai.com>
2022-07-16 18:56:12 -0300

    SQL andando

--- gm/index.js
+++ gm/index.js
@@ -1,79 +1,75 @@
-const express = require('express');
-const app= express(); 
-require ('dotenv').config(); 
-const Port = process.env.PORT || 8080; 
-const hbs = require('hbs');
-const mysql= require('mysql2');
-const path= require('path'); 
-const nodemailer= require('nodemailer'); 
-const { restart } = require('nodemon');
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const Port = process.env.PORT || 8080;
+const hbs = require("hbs");
+const mysql = require("mysql2");
+const path = require("path");
+const nodemailer = require("nodemailer");
+const { restart } = require("nodemon");
 
 //Conectamos la app a una base de datos
 const conexion = mysql.createConnection({
-    host: process.env.HOST, 
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    port: process.env.PORTDB, 
-    database: process.env.DATABASE,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.PORTDB,
+  database: process.env.DATABASE,
 });
 
 // Conectamos la DB
-const conectar = (
-        conexion.connect((error)=>{
-        if(error) throw error;
-        console.log('Base de Datos Conectada!!');
-    }) 
-);
+const conectar = conexion.connect((error) => {
+  if (error) throw error;
+  console.log("Base de Datos Conectada!!");
+});
 
 //Configuración de Middelwares (funciones por las cuales pasan todos los datos que entran y salen de determinada forma)
 app.use(express.json());
 /* app.use(express.static(path.join(__dirname, "public"))); */
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
-
-app.get('/', (req, res) =>{
-    res.render('index', {titulo:'Formulario para completar'})
+app.get("/", (req, res) => {
+  res.render("index", { titulo: "Formulario para completar" });
 });
 
-app.get('/abogados', (req, res) =>{
-    res.render('abogados' )
+app.get("/abogados", (req, res) => {
+  res.render("abogados");
 });
 
-app.get('/areasdepractica', (req, res) =>{
-    res.render('areasdepractica')
+app.get("/areasdepractica", (req, res) => {
+  res.render("areasdepractica");
 });
 
-app.get('/nuestroestudio', (req, res) =>{
-    res.render('nuestroestudio')
+app.get("/nuestroestudio", (req, res) => {
+  res.render("nuestroestudio");
 });
 
-app.get('/contacto', (req, res) =>{
-    res.render('contacto')
+app.get("/contacto", (req, res) => {
+  res.render("contacto");
 });
 
-  // VERBO HTTP PARA RECIBIR DATOS
-app.post('/contacto', (req, res)=>{
+// VERBO HTTP PARA RECIBIR DATOS
+app.post("/contacto", (req, res) => {
+    console.log("estoy corriendo")
+  console.log("req", req.body);
 
-    console.log(req.body);
+  const { nombre, apellido, mail, consulta } = req.body;
 
-const {nombre, apellido, mail, consulta} = req.body;
+  //Validacion basica
 
+  let validacion = "Faltan datos para dejar la consulta";
 
-//Validacion basica
-
-let validacion = 'Faltan datos para dejar la consulta'
-
-if(nombre == "" || mail == ""){
-res.render('contacto', {
-    titulo:'Faltan datos',
-    validacion
-})
-}else{
+  if (nombre == "" || mail == "") {
+    res.render("contacto", {
+      titulo: "Faltan datos",
+      validacion,
+    });
+  } else {
     console.log(nombre);
     console.log(apellido);
     console.log(mail);
@@ -81,41 +77,35 @@ res.render('contacto', {
 
     //conectar()
 
-let data ={
-    producto_id: id,
-    cliente_nombre: nombre,
-    cliente_apellido: apellido,
-    cliente_correo: mail,
-    cliente_mensaje: consulta
-}
+    let data = {
+      cliente_nombre: nombre,
+      cliente_apellido: apellido,
+      cliente_correo: mail,
+      cliente_mensaje: consulta,
+    };
+    console.log("nombre", nombre)
+    let sql = "INSERT INTO CLIENTE SET ?";
 
+    let query = conexion.query(sql, data, (error, results) => {
+      if (error) throw error;
+      res.render("Contacto", { titulo: "Formulario para completar" });
+    });
+  }
+});
 
-let sql = "INSERT INTO CLIENTE SET ?";
+app.get("/contacto", (req, res) => {
+  let sql = "SELECT * FROM CLIENTE";
 
-let query = conexion.query(sql, data, (error, results) =>{
+  let query = conexion.query(sql, (error, results) => {
     if (error) throw error;
-    res.render('Contacto', {titulo:'Formulario para completar'})
-});
-}
-});
-
-app.get('/contacto', (req, res) =>{
-
-    let sql = "SELECT * FROM CLIENTE";
-
-    let query = conexion.query(sql, (error, results) =>{
-        if (error) throw error;
-        res.render('contacto', {titulo:'Joia',
-        results})
-    });   
+    res.render("contacto", { titulo: "Joia", results });
+  });
 });
 
-app.listen(Port, ()=>{
-    console.log(`Servidor corriendo en el Puerto ${Port}`);
-    
-    })
+app.listen(Port, () => {
+  console.log(`Proyecto corriendo en el Puerto ${Port}`);
+});
 
-    app.on('error', (error) =>{
-        console.log(`Tenemos un error ${error}`);
-        
-        })
+app.on("error", (error) => {
+  console.log(`Tenemos un error ${error}`);
+});
